edebdbb7ca3c578a1fe04177c7c243060e4914d1
jonathanarg <jargov@gmail.com>
2017-10-18 16:33:57 +0300

    Create confEmptySuites.ts

---
+++ exampleTypescript/confEmptySuites.ts
@@ -0,0 +1,36 @@
+// Because this file imports from  protractor, you'll need to have it as a
+// project dependency. Please see the reference config: lib/config.ts for more
+// information.
+//
+// Why you might want to create your config with typescript:
+// Editors like Microsoft Visual Studio Code will have autocomplete and
+// description hints.
+//
+// To run this example, first transpile it to javascript with `npm run tsc`,
+// then run `protractor conf.js`.
+import {Config} from 'protractor';
+
+export let config: Config = {
+  framework: 'jasmine',
+  capabilities: {
+    browserName: 'chrome'
+  },
+//   specs: ['spec.ts',
+//          'spec2.ts'],
+suites: {
+   	logIn2: [
+ 	  'spec111.ts'
+ 	  ],
+         logInSaas2: 'spec.ts',
+         releasesPageObject2: ['0spec.ts',
+			     'spec2.ts',
+ 			     'spec3.ts']
+ },
+	
+
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+
+  // You could set no globals to true to avoid jQuery '$' and protractor '$'
+  // collisions on the global namespace.
+  noGlobals: true
+};
